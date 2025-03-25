9d1db963e8da08b7a575073c11a0afcfacf427be
ii-tech <154016576+ii-tech@users.noreply.github.com>
2025-03-25 08:55:25 +0530

    Update FlutterNativeImagePlugin.java

--- android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
+++ android/src/main/java/com/example/flutternativeimage/FlutterNativeImagePlugin.java
@@ -1,11 +1,56 @@
+// package com.example.flutternativeimage;
+
+// import android.content.Context;
+
+// import io.flutter.embedding.engine.plugins.FlutterPlugin;
+// import io.flutter.plugin.common.BinaryMessenger;
+// import io.flutter.plugin.common.MethodChannel;
+// import io.flutter.plugin.common.PluginRegistry;
+
+// /**
+//  * FlutterNativeImagePlugin
+//  */
+// public class FlutterNativeImagePlugin implements FlutterPlugin {
+//   private static final String CHANNEL_NAME = "flutter_native_image";
+//   private MethodChannel channel;
+//   /**
+//    * Plugin registration.
+//    */
+//   public static void registerWith(PluginRegistry.Registrar registrar) {
+//     final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
+//     plugin.setupChannel(registrar.messenger(), registrar.context());
+//   }
+
+//   @Override
+//   public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
+//     setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
+//   }
+
+//   @Override
+//   public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
+//     teardownChannel();
+//   }
+
+//   private void setupChannel(BinaryMessenger messenger, Context context) {
+//     channel = new MethodChannel(messenger, CHANNEL_NAME);
+//     MethodCallHandlerImpl handler = new MethodCallHandlerImpl(context);
+//     channel.setMethodCallHandler(handler);
+//   }
+
+//   private void teardownChannel() {
+//     channel.setMethodCallHandler(null);
+//     channel = null;
+//   }
+// }
+
 package com.example.flutternativeimage;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * FlutterNativeImagePlugin
@@ -13,21 +58,14 @@ import io.flutter.plugin.common.PluginRegistry;
 public class FlutterNativeImagePlugin implements FlutterPlugin {
   private static final String CHANNEL_NAME = "flutter_native_image";
   private MethodChannel channel;
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(PluginRegistry.Registrar registrar) {
-    final FlutterNativeImagePlugin plugin = new FlutterNativeImagePlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext());
   }
 
   @Override
-  public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
-    setupChannel(binding.getFlutterEngine().getDartExecutor(), binding.getApplicationContext());
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPlugin.FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     teardownChannel();
   }
 
@@ -38,7 +76,10 @@ public class FlutterNativeImagePlugin implements FlutterPlugin {
   }
 
   private void teardownChannel() {
-    channel.setMethodCallHandler(null);
-    channel = null;
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
   }
 }
+
